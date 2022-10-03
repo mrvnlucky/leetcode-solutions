64b6fbc0f05000f9aa53923a2624adb923fa1e8f
Marvin Luckianto <marvnluck@gmail.com>
2022-10-03 21:21:28 +0700

    Time: 8251 ms (5.01%), Space: 15.2 MB (91.72%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -7,9 +7,8 @@ class Solution:
         for i in range(len(nums)):
             # start with leftArr(0:) then to (0:-1) and so on
             # or with rightArr(:-1) then to (1:-1)
-            # print(rightArr[i+1:])
-            # print(leftArr[:i])
+
             if sum(rightArr[i+1:]) == sum(leftArr[:i]):
-                # print(i)
+
                 return i
         return -1
