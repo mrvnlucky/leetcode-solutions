939d644a42ee09cde05487140091e67b79827611
Marvin Luckianto <marvnluck@gmail.com>
2024-04-05 01:05:01 +0700

    Time: 2 ms (74.41%), Space: 2.5 MB (7.08%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.go
@@ -0,0 +1,17 @@
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+    max := 0
+    for _, candy := range candies {
+        if candy > max {
+            max = candy
+        }   
+    }
+    result := make([]bool, len(candies))
+    for i, candy := range candies {
+        if candy + extraCandies >= max {
+            result[i] = true
+        } else {
+            result[i] = false
+        }
+    }
+    return result
+}
