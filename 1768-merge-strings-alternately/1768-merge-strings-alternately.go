20af45068668229c7679c09c5707d834e9a326b9
Marvin Luckianto <marvnluck@gmail.com>
2024-04-04 23:23:29 +0700

    Time: 0 ms (100.00%), Space: 3 MB (10.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,6 +1,10 @@
 func mergeAlternately(word1 string, word2 string) string {
     res := ""
-    for i := 0; i < len(word1) || i < len(word2); i++ {
+    maxLen := len(word1)
+    if len(word1) < len(word2) {
+        maxLen = len(word2)
+    }
+    for i := 0; i < maxLen; i++ {
         if i < len(word1) {
             res += string(word1[i])
         }
